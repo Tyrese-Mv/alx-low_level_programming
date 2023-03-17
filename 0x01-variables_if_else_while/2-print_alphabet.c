e814671d7014ef9f8cf95c3073a78a6ae910244e
Tyrese-Mv <musawenkosi106@gmail.com>
2023-03-17 12:33:09 -0700

    Added task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch != 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
