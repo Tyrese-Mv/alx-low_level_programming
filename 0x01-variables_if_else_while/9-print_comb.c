619d9997d6b01f906c9668d5b73b20b8bddcb68b
Tyrese-Mv <musawenkosi106@gmail.com>
2023-03-18 06:56:31 -0700

    Added task 9 fix 2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,17 @@ int main(void)
 {
 	int num = '0';
 
-	while (num <= '9')
+	while (num <= '8')
 	{
 		putchar(num);
 		putchar(',');
 		putchar(' ');
 		num++;
+	}
+	if (num == '8')
+	{
+		num++;
+		putchar(num);
 	}
 	putchar('\n');
 	return (0);
