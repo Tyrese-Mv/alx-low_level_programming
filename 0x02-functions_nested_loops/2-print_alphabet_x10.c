dd2d95d1d715e830ce61dba6ff7888be23e8051b
Tyrese-Mv <musawenkosi106@gmail.com>
2023-03-21 14:22:50 -0700

    added task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print set of alphabets ten times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch;
+
+	for (i = 1; i <= 10; i++)
+	{
+		ch = 'a';
+		while (ch != 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+	}
+}
