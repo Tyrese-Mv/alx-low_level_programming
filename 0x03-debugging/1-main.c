ae2ff770e96490da8648904efeba3ebb3f846961
Tyrese-Mv <musawenkosi106@gmail.com>
2023-03-24 11:36:11 -0700

    Added task 1 fix 2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -15,6 +17,8 @@ int main(void)
 	/*{*/
 	/*putchar(i);*/
 	/*}*/
+
 	printf("Infinite loop avoided! \\o/\n");
+	
 	return (0);
 }
