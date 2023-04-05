99f502057b4ece80cd17ec2db0a9936cd5ebf158
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-05 14:15:01 -0700

    Added task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - Checks occurence of characters
+ * @s: string to check
+ * @c: starts printing from this character
+ * Return: c
+ */
+char *_strchr(char *s, char c)
+{
+	int j = 0;
+
+	while (s[j] <= '\0')
+	{
+		if (s[j] == c)
+		{
+			return (s + j);
+		}
+		j++;
+	}
+	return (NULL);
+}
