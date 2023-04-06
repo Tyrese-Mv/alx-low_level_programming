b910c6aede426df8d39faaceecca2e546d463b03
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-06 08:47:09 -0700

    Added task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,16 @@
-test
+#include "main.h"
+/**
+ * _puts_recursion - prints string
+ * @s: string to print
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
