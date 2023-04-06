2429af933da5a4b5bfb2c124f21427c1d41e7afa
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-06 09:57:08 -0700

    Added task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * factorial - multipliyng number by itself minus 1
+ * @n: number to calculate
+ * Return: num
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (n == 0)
+		{
+			return (1);
+		}
+		else
+		{
+			return (factorial(n) * factorial(n - 1));
+		}
+	}
+}
