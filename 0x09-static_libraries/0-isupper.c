b141241ea635797eaffaf0a2d58a5a2a22d41a30
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-09 05:38:07 -0700

    first batch task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - returns lower case
+ * @c: integer
+ * Return: 1
+ */
+int _islower(int c)
+{
+	return (c);
+}
---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _memset - set memory
+ * @s: s
+ * @b: b
+ * @n: integer
+ * Return: pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
