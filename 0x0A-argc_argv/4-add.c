669a7f98795740a48c2f013494487daccdf45cec
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-10 13:49:43 -0700

    added task 4 fix 9

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - add numbers
  * @argc: count
@@ -17,7 +18,7 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (((*argv[i] < 48) && (*argv[i] > 58)))
+		if (!isdigit(*argv[i]))
 		{
 			printf("Error\n");
 			return (1);
