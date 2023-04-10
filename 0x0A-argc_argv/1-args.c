74e6e31e638daf582b50abb9dee2a9469ed665e3
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-10 06:32:51 -0700

    added task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - returns total num arguments
+ * @argc: arg count
+ * @argv: arg vectors
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
