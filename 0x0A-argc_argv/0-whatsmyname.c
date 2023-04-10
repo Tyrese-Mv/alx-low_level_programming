01a650a13624a5b89c36a59f51258debf4e881ad
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-10 05:26:06 -0700

    added folder and task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stio.h>
+/**
+ * main - outputs first argument
+ * @argc: argument count
+ * @argv: argument variable
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
