ee9969631b1a3f651e5b2fa6856f2cfa16290d99
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-10 12:12:33 -0700

    added task 3 fix 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - add numbers
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
