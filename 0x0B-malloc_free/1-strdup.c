8ae15dfe9e3bc94603761604e4ed921a03acf95a
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-11 12:20:37 -0700

    added task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strlen - get srting length
+ * @strnew: string to count
+ * Return: string length
+ */
+int _strlen(char *strnew)
+{
+	int i = 0;
+
+	while (strnew[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strdup - duplicate a string function
+ * @str: new string
+ * Return: string
+ */
+char *_strdup(char *str)
+{
+	int i, num = sizeof(char) * _strlen(str);
+	char *newchar;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	newchar = malloc(num);
+	for (i = 0; i < num; i++)
+	{
+		newchar[i] = str[i];
+	}
+	return (newchar);
+}
