4d499466e68f11adef6418e13689421913fd020a
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-12 00:59:37 -0700

    added task 2 fix 1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,11 +22,19 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	while (s1[i] != '\0')
 	{
+		if (s1 == NULL)
+		{
+			break;
+		}
 		i++;
 	}
 	j = 0;
 	while (s2[j] != '\0')
 	{
+		if (s2 == NULL)
+		{
+			break;
+		}
 		j++;
 	}
 	ch = malloc(sizeof(char) * i + j + 1);
