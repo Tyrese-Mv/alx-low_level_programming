e007c2578ae32a95b1c3f2995ed87731142cbd0a
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-13 10:07:23 -0700

    added task 1 fix 6

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,11 +24,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		j++;
 	if (n >= j)
 	{
-		ch = malloc(sizeof(char) * (i + n));
+		ch = malloc(sizeof(char) * (i + n + 1));
 	}
 	else
 	{
-		ch = malloc(sizeof(char) * (i + j));
+		ch = malloc(sizeof(char) * (i + j + 1));
 	}
 	if (ch == NULL)
 		return (NULL);
