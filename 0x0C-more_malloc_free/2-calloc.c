6094568099f918d6fe05cf9dacd937c9a39993bc
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-13 13:40:46 -0700

    added task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - Allocate memory set to zero
+ * @nmemb: number of elements
+ * @size: size of elements
+ * Return: array pointer or null
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, j;
+	char *ch;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	j = sizeof(size) * nmemb;
+	ch = malloc(j);
+	if (ch == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < j; i++)
+	{
+		ch[i] = 0;
+	}
+	return (ch);
+}
