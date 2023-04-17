e0ff2fa553f1c4fdb68907687f80e2add45029a6
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-17 15:33:28 -0700

    added task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,13 @@
  * @age: dog age
  * @owner: dog owner
  */
-struct dog
+typedef struct
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
+typedef dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
