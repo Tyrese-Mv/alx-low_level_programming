da305fbb05a2c7f157a602827d0b5b5bcef314be
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-18 03:25:04 -0700

    added task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * is_prime_number - checks for prime number
+ * @n: numner to check
+ * Return: recursive fuction to check prime numbers
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (!check_divisible(n, n - 1));
+}
+
+/**
+ * check_divisible - checks if n is divisale by i
+ * @n: n from is prime function
+ * @i: i is n - 1
+ * Return: check divisible recursively
+ */
+int check_divisible(int n, int i)
+{
+	if (i == 1)
+		return (0);
+	if (n % i == 0)
+		return (1);
+	return (check_divisible(n, i - 1));
+}
