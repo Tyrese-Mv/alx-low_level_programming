d7ebf3ab5e5479e1e8a469c2dd0fd7a299b02379
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-18 01:43:41 -0700

    added task 5 fix 1

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,10 +11,10 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	i = _sqrt_recursion(n / 4) * 2;
-	if (x * x <= n && (x + 1) > n)
+	if (i * i <= n && (i + 1) > n)
 		return (i);
-	else if ((x + 1) * (x + 1) == n)
-		return (x + 1);
+	else if ((i + 1) * (i + 1) == n)
+		return (i + 1);
 	else
 		return (_sqrt_recursion(n));
 }
