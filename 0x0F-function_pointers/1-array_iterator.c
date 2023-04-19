c11f1e006073c9b46808a0fdc02487c4108fbe04
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-19 08:12:45 -0700

    added task 1 fix 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,8 +10,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
-
+	unsigned int i;
+	
+	if (array == NULL || action == NULL)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
