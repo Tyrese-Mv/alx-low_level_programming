f425ed6ef9aad05ce82927ba8888c38b7fef561e
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-19 07:19:32 -0700

    added task 0 fix 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 /**
- * f - prints name
- * @*: char parameter to be printed
+ * print_name - prints name
+ * @name: name to be printed
+ * @f: char parameter to be printed
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
