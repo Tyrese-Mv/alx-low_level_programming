6647b30b10fa3473552191ae61a9a25f9b599686
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-20 14:23:32 -0700

    added task 2 fix 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", ch);
 		}
-		if (separator != NULL && i != n)
+		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
