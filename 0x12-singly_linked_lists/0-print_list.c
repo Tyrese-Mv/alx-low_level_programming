1c15070cb9fb4eb359ed8933d9ec262ce8015537
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-28 10:50:50 -0700

    added task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_list - prints the number of elements in the  linked list
+ * @h: heard parameter
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("%s\n", h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This folder is for Singly linked lists tasks
