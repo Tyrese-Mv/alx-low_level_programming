8d802f3e83423ec48bbe960680aae6b8b0ddc084
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-28 10:58:52 -0700

    added task 0 fix 3

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n",count, h->str);
+			printf("[%d] %s\n", count, h->str);
 		h = h->next;
 		count++;
 	}
