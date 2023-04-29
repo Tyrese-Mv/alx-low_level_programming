91f54dd55a43cc47b54971cd6d1750cc946b7e32
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-29 01:36:25 -0700

    added task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * before_main - Prints before main function executes
+ * Return: void
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,5 +23,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 unsigned int _strlen(char *str);
-
+void before_main(void) __attribute__ ((constructor));
 #endif
