6c92c7648a7ab33c03805583bf70ef1de59a75cb
Tyrese-Mv <musawenkosi106@gmail.com>
2023-05-03 03:23:57 -0700

    added task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * listint_len - checks numer of elements in a linked list
+ * @h: nodes to check
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *current = h;
+	size_t i = 0;
+
+	if (current == NULL)
+		return (-1);
+	if (current != NULL)
+	{
+		i++;
+		current = current->next;
+	}
+	return (i);
+}
