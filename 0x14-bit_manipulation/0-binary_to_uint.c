baec8b05b91bf0188504ada0832ef47c7f4745bc
Tyrese-Mv <musawenkosi106@gmail.com>
2023-07-06 11:02:58 -0700

    added task 0 fix 3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '1' && b[1] != '0')
+		if (b[i] != '1' && b[i] != '0')
 			return (0);
 		sum = (sum << 1) + (*b - '\0');
 	}
