136641b3555cda09b7d2e58562ceba1ee868fc80
Tyrese-Mv <musawenkosi106@gmail.com>
2024-02-08 15:16:44 +0200

    added task 0 fix 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,16 +6,16 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int res = 0;
-    if (b == NULL)
-        return (0);
-    
-    while (*b != '\0')
-    {
-        if (*b != '0' && *b != '1')
-            return (0);
-        res = (res << 1) + (*b - '0');
-        b++;
-    }
-    return res;
+	unsigned int res = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		res = (res << 1) + (*b - '0');
+		b++;
+	}
+	return (res);
 }
