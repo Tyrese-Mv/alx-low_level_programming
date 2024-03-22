5efebdfcf4581b25882e28b2ec20acb5ae4b6487
Tyrese-Mv <musawenkosi106@gmail.com>
2024-03-22 11:17:16 +0200

    added 6

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - gets nth node
+ * @head: first node
+ * @index: index of node to return
+ * Return: node or NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *current;
+
+	if (head == NULL)
+		return (NULL);
+	current = head;
+	for (i = 0; current != NULL; i++)
+	{
+		if (i == index)
+			return (current);
+		current = current->next;
+	}
+	return (NULL);
+}
